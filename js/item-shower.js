7ebb0db4b9024993798311a4f10379dcb13c618d
Said Tashmanov <alright.said.offcl@gmail.com>
2019-11-06 16:52:45 +0600

    Tile's site

---
+++ js/item-shower.js
@@ -0,0 +1,160 @@
+// 1
+$('#1').click(function() {
+    console.log('w');
+    $('#sh1').toggleClass('hide');
+    return false;
+})
+$('#sh1').click(function() {
+    $('#sh1').toggleClass('hide');
+})
+
+// 2
+$('#2').click(function() {
+    console.log('w');
+    $('#sh2').toggleClass('hide');
+    return false;
+})
+$('#sh2').click(function() {
+    $('#sh2').toggleClass('hide');
+})
+
+// 3
+$('#3').click(function() {
+    console.log('w');
+    $('#sh3').toggleClass('hide');
+    return false;
+})
+$('#sh3').click(function() {
+    $('#sh3').toggleClass('hide');
+})
+
+// 4
+$('#4').click(function() {
+    console.log('w');
+    $('#sh4').toggleClass('hide');
+    return false;
+})
+$('#sh4').click(function() {
+    $('#sh4').toggleClass('hide');
+})
+
+// 5
+$('#5').click(function() {
+    console.log('w');
+    $('#sh5').toggleClass('hide');
+    return false;
+})
+$('#sh5').click(function() {
+    $('#sh5').toggleClass('hide');
+})
+
+
+// 6
+$('#6').click(function() {
+    console.log('w');
+    $('#sh6').toggleClass('hide');
+    return false;
+})
+$('#sh6').click(function() {
+    $('#sh6').toggleClass('hide');
+})
+
+// 7
+$('#7').click(function() {
+    console.log('w');
+    $('#sh7').toggleClass('hide');
+    return false;
+})
+$('#sh7').click(function() {
+    $('#sh7').toggleClass('hide');
+})
+
+// 8
+$('#8').click(function() {
+    console.log('w');
+    $('#sh8').toggleClass('hide');
+    return false;
+})
+$('#sh8').click(function() {
+    $('#sh8').toggleClass('hide');
+})
+
+// 9
+$('#9').click(function() {
+    console.log('w');
+    $('#sh9').toggleClass('hide');
+    return false;
+})
+$('#sh9').click(function() {
+    $('#sh9').toggleClass('hide');
+})
+
+// 10
+$('#10').click(function() {
+    console.log('w');
+    $('#sh10').toggleClass('hide');
+    return false;
+})
+$('#sh10').click(function() {
+    $('#sh10').toggleClass('hide');
+})
+
+// 11
+$('#11').click(function() {
+    console.log('w');
+    $('#sh11').toggleClass('hide');
+    return false;
+})
+$('#sh11').click(function() {
+    $('#sh11').toggleClass('hide');
+})
+
+// 12
+$('#12').click(function() {
+    console.log('w');
+    $('#sh12').toggleClass('hide');
+    return false;
+})
+$('#sh12').click(function() {
+    $('#sh12').toggleClass('hide');
+})
+
+// 13
+$('#13').click(function() {
+    console.log('w');
+    $('#sh13').toggleClass('hide');
+    return false;
+})
+$('#sh13').click(function() {
+    $('#sh13').toggleClass('hide');
+})
+
+// 14
+$('#14').click(function() {
+    console.log('w');
+    $('#sh14').toggleClass('hide');
+    return false;
+})
+$('#sh14').click(function() {
+    $('#sh14').toggleClass('hide');
+})
+
+// 15
+$('#15').click(function() {
+    console.log('w');
+    $('#sh15').toggleClass('hide');
+    return false;
+})
+$('#sh15').click(function() {
+    $('#sh15').toggleClass('hide');
+})
+
+// 16
+$('#16').click(function() {
+    console.log('w');
+    $('#sh16').toggleClass('hide');
+    return false;
+})
+$('#sh16').click(function() {
+    $('#sh16').toggleClass('hide');
+})
